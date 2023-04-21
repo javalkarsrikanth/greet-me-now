ba4931c44bd8eab620c8fac8b553afad176e760a
Srikanth <srikanthjavalkar@gmail.com>
2023-04-21 12:48:58 +0530

    Refactored code

--- src/main/java/com/poc/GreetMeNowApplication.java
+++ src/main/java/com/poc/GreetMeNowApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class WebHookApplication {
+public class GreetMeNowApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(WebHookApplication.class, args);
+		SpringApplication.run(GreetMeNowApplication.class, args);
 	}
 
 }
---
+++ src/main/java/com/poc/controller/GreetMeNowApplication.java
@@ -0,0 +1,12 @@
+package com.poc.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class GreetMeNowApplication {
+
+    @GetMapping("/greet")
+    public String sayHello(){
+        return "Hello Dear :) How are you ?";
+    }
+}
